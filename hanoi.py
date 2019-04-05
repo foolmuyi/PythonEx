9f5d2c84b3b5a5397c3d7d2e6e34c931393f3a30
muyi <muyiorlk@gmail.com>
2019-04-05 20:08:02 +0800

    tower of Hanoi

---
+++ hanoi.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def move(n,a,b,c):
+	if n == 1:
+		print(a,'-->',c)
+		return
+	else:
+		move(n-1,a,c,b)
+		print(a,'-->',c)
+		move(n-1,b,a,c)
+
+
+move(3,'A','B','C')
